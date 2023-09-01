b109df8e21bfb6dc38513367310ad7c5171cf9f2
tonyho <TonyHo@via.com.tw>
2023-09-01 11:45:50 +0800

    remove double-quote is ok

--- folder_path/folder.sh
+++ folder_path/folder.sh
@@ -3,19 +3,62 @@
 set -x # echo on for debug
 
 # setup your main floder(MF)
-MF="/home/tonyho/workspace/fsic"
+MF="/home/tonyho/workspace/debug"
 
-CLF="$MF/caravel-lab"
-CUPF="$MF/caravel_user_project"
+LF="$MF/folder_test_log"
+CLF="$MF/test1"
+CUPF="$MF/test2"
+
+if [ ! -d "$MF" ];
+then
+    mkdir $MF
+fi
 
 cd $MF
 pwd
+
+
+
+
+if [ -d "$LF" ];
+then
+    rm -rf $LF
+fi
+mkdir $LF
+
+if [ -d "$CLF" ];
+then
+    rm -rf $CLF
+fi
+mkdir $CLF
+
+if [ -d "$CUPF" ];
+then
+    rm -rf $CUPF
+fi
+mkdir $CUPF
+
+ls  2>&1 | tee "$LF/ls.log"
+
 
 cd $CLF
 pwd
 
 cd $CUPF
 pwd
+
+
+echo "with double-quote when using varaiable"
+cp -f "$LF/ls.log"  "$CLF/ls-caravel-lab.log"
+ls "$CLF"
+cat "$CLF/ls-caravel-lab.log"
+
+echo "without double-quote when using variable"
+cp -f $LF/ls.log  $CUPF/ls-caravel_user_project.log
+ls $CUPF
+cat $CUPF/ls-caravel_user_project.log
+
+
 
 # setup your main floder - this case is fail BMF for bad main folder
 BMF="~/workspace/fsic"
